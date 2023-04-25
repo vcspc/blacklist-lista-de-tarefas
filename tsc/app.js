85a3f436cd1ebaf0954103c3afaceb90a3098fe7
vinicius costa <vitonilcius@gmail.com>
2023-04-24 23:50:02 -0300

    Assunto: Termino do script Descrição: - Adicionado a função do botão adiconar tarefa - Adicionado a função atulaizarListaTarefas

--- tsc/app.js
+++ tsc/app.js
@@ -26,4 +26,34 @@ const corpoDasTarefas = document.querySelector('.tarefas');
 const inputNovaTarefa = document.querySelector('#nova-tarefa');
 const botaoAdicionarTarefa = document.querySelector('#adicionar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
-console.log(inputNovaTarefa, botaoAdicionarTarefa, listaTarefas);
+let lista = new ListaDeTarefas();
+botaoAdicionarTarefa.addEventListener('click', () => {
+    let descricao = inputNovaTarefa.value;
+    let tarefa = new Tarefa(descricao);
+    lista.adicionarTarefa(tarefa);
+    inputNovaTarefa.value = '';
+    atualizarListaTarefas();
+});
+function atualizarListaTarefas() {
+    listaTarefas.innerHTML = '';
+    lista.tarefas.forEach((tarefa, index) => {
+        let li = document.createElement('li');
+        let tarefaSpan = document.createElement('span');
+        tarefaSpan.innerText = tarefa.descricao;
+        tarefaSpan.classList.add('tarefas__tarefa__texto');
+        tarefaSpan.addEventListener('click', () => {
+            tarefa.toggleConcluida();
+            atualizarListaTarefas();
+        });
+        li.appendChild(tarefaSpan);
+        let botaoRemover = document.createElement('button');
+        botaoRemover.innerText = '<i class="fa-regular fa-trash-can fa-sm"></i>';
+        botaoRemover.classList.add('tarefas__tarefa__acao__excluir');
+        botaoRemover.addEventListener('click', () => {
+            lista.removerTarefa(index);
+            atualizarListaTarefas();
+        });
+        li.appendChild(botaoRemover);
+        listaTarefas.appendChild(li);
+    });
+}
--- tsc/app.ts
+++ tsc/app.ts
@@ -37,4 +37,36 @@ const inputNovaTarefa = document.querySelector('#nova-tarefa')! as HTMLInputElem
 const botaoAdicionarTarefa = document.querySelector('#adicionar-tarefa')! as HTMLButtonElement;
 const listaTarefas = document.querySelector('#lista-tarefas')! as HTMLUListElement;
 
-console.log(inputNovaTarefa, botaoAdicionarTarefa, listaTarefas);
+let lista = new ListaDeTarefas();
+
+botaoAdicionarTarefa.addEventListener('click', () => {
+    let descricao = inputNovaTarefa.value;
+    let tarefa = new Tarefa(descricao);
+    lista.adicionarTarefa(tarefa);
+    inputNovaTarefa.value = '';
+    atualizarListaTarefas();
+});
+
+function atualizarListaTarefas() {
+    listaTarefas.innerHTML = '';
+    lista.tarefas.forEach((tarefa, index) => {
+        let li = document.createElement('li');
+        let tarefaSpan = document.createElement('span');
+        tarefaSpan.innerText = tarefa.descricao;
+        tarefaSpan.classList.add('tarefas__tarefa__texto');
+        tarefaSpan.addEventListener('click', () => {
+            tarefa.toggleConcluida();
+            atualizarListaTarefas();
+        });
+        li.appendChild(tarefaSpan);
+        let botaoRemover = document.createElement('button');
+        botaoRemover.innerText = '<i class="fa-regular fa-trash-can fa-sm"></i>';
+        botaoRemover.classList.add('tarefas__tarefa__acao__excluir');
+        botaoRemover.addEventListener('click', () => {
+            lista.removerTarefa(index);
+            atualizarListaTarefas();
+        });
+        li.appendChild(botaoRemover);
+        listaTarefas.appendChild(li);
+    });
+}
